33d2bf23e7db84478ad5e8f8aeed21accb8e41f3
coihsan <110887888+coihsan@users.noreply.github.com>
2024-01-05 00:21:42 +0000

    Update 4 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main className="bg-black">{children}</main>
       </body>
     </html>
   );
