8c2532b041f23727edc5800d05e6479a0f4848ac
coihsan <110887888+coihsan@users.noreply.github.com>
2024-01-07 01:34:25 +0000

    Update Hero.tsx and Skills.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -30,20 +30,20 @@ const Hero = () => {
         <ButtonLink
           link={"#"}
           variant={
-            "bg-gray-300 dark:bg-zinc-600 border-gray-400/40 dark:border-gray-500 dark:fill-white"
+            "bg-slate-300 dark:bg-zinc-600 border-gray-400/40 dark:border-gray-500 dark:fill-white"
           }
           text={"Resume"}
           icon={iconsUI.link}
         />
       </div>
-      <div className="mt-[2rem] relative px-8 rounded-lg border bg-slate-50 dark:bg-zinc-900 border-zinc-400 dark:border-zinc-600 shadow-md">
+      <div className="mt-[2rem] relative px-8 rounded-lg border bg-slate-100 dark:bg-zinc-900 border-zinc-400 dark:border-zinc-600 shadow-md">
         <code className="absolute top-0 left-0 p-2 rounded-t-lg text-sky-500 w-full border-b-[1px] border-zinc-400 dark:border-zinc-600 pl-8 text-[14px]">
           about_me.md
         </code>
         <p className="pt-[4rem] text-[18px] dark:text-zinc-400">
           {personal.about}
         </p>
-        <div className="flex items-center gap-2 py-5 max-sm:flex-col max-sm:items-start">
+        <div className="flex items-center gap-2 py-1 max-sm:flex-col max-sm:items-start">
           <h3>Credentials</h3>
           <div className="flex items-center gap-2 flex-wrap">
             <div className="w-12 h-12">{iconsUI.skillshop}</div>
@@ -61,19 +61,19 @@ const Hero = () => {
             </a>
           </div>
         </div>
-        <div className="my-[2rem]">
-        <div className="flex itemx-center flex-wrap gap-2">
-          {personal.passion.map((skill) => (
-            <div
-              key={skill.key}
-              className="flex items-center gap-1 px-3 py-1 shadow-md rounded-full border border-zinc-400 dark:border-zinc-600"
-            >
-              <span className="dark:fill-zinc-100">{skill.icon}</span>
-              <p className="font-medium max-sm:text-sm">{skill.skilled}</p>
-            </div>
-          ))}
+        <div className="mb-[2rem]">
+          <div className="flex itemx-center flex-wrap gap-2">
+            {personal.passion.map((skill) => (
+              <div
+                key={skill.key}
+                className="flex items-center gap-1 px-3 py-1 shadow-md rounded-full border border-zinc-400 dark:border-zinc-600"
+              >
+                <span className="dark:fill-zinc-100">{skill.icon}</span>
+                <p className="font-medium max-sm:text-sm">{skill.skilled}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -3,7 +3,7 @@ import { iconsRepo, dataplatform } from "../constants/IconsRepo";
 
 export default function Skills() {
   return (
-    <section className="shadow-md bg-zinc-100 dark:bg-zinc-900 p-5 rounded-lg border border-zinc-400 dark:border-zinc-800">
+    <section className="shadow-md bg-slate-100 dark:bg-zinc-900 p-5 rounded-lg border border-zinc-400 dark:border-zinc-800">
       <h1 className="titleSection text-center">Tech Stack</h1>
       <ul className="grid grid-cols-4 gap-2 max-sm:grid-cols-3">
         {iconsRepo.map((icons) => (
